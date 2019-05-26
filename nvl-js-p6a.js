75594813b508d0d07a7338bff583f317f8d6acf1
Felix Nogales <felixnoga@gmail.com>
2019-05-26 21:03:12 +0200

    Script con variables

---
+++ nvl-js-p6a.js
@@ -0,0 +1,6 @@
+var nombre = "Félix";
+var apellidos = "Nogales Quintana";
+var nombre_apellidos = nombre + " " + apellidos;
+var edad = 37;
+var gravedad = 9.8;
+var tres_primeros_impares = [1, 3, 5];
