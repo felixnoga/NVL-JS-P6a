4c14a186f58c5751818351c38716b61afc87ef1d
Felix Nogales <felixnoga@gmail.com>
2019-05-26 21:05:11 +0200

    Añadidos dias de la semana

--- nvl-js-p6a.js
+++ nvl-js-p6a.js
@@ -1,6 +1,7 @@
 var nombre = "Félix";
 var apellidos = "Nogales Quintana";
 var nombre_apellidos = nombre + " " + apellidos;
+var dias_semana = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
 var edad = 37;
 var gravedad = 9.8;
 var tres_primeros_impares = [1, 3, 5];
